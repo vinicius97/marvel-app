1e4abe72f92a52b17c2b457a8c922800ae22f5c3
Vinicius Lana <contato@viniciuslana.com>
2019-08-30 01:40:56 -0300

    Add edit button to details character

--- src/routes/Characters/Details/Details.js
+++ src/routes/Characters/Details/Details.js
@@ -1,15 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 // Components
 import { Card } from '../../../components'
 
+// Constants
+import { CharactersRoutes } from '../../../constants/Routes'
+
 export function Details ({ character }) {
-  const { thumbnail, name, series } = character
+  const { thumbnail, name, series, id } = character
   const thumbnailSrc = thumbnail && `${thumbnail.path}.${thumbnail.extension}`
 
   return (
     <div>
       <Card thumbnail={thumbnailSrc} title={name} />
+
+      <Link to={`${CharactersRoutes.edit.replace(':id', id)}`}>
+        <div> Editar </div>
+      </Link>
 
       Series
       {series && series.items.map((serie, key) => (
